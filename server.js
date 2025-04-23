ffb86de3bbc627294ed359da2c47fbedbbde08d6
Kapeesh Selvathangaraj <kapeeshkapeesh66@gmail.com>
2025-04-23 14:56:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,5 +11,6 @@ mongoose.connect(process.env.MONGO_URI);
 
 app.use('/api/sensor-data', require('./routes/sensorData'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
